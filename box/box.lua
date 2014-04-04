56597445ac151747cc283d3a045a89dc1d442738
Seamus Tuohy <s2e@opentechinstitute.org>
2014-04-04 14:53:50 -0400

    Finally spent the 45 minutes needded to get this thing to work.

--- box/box.lua
+++ box/box.lua
@@ -1,67 +1,60 @@
-module("luci.controller.pirate.box", package.seeall)
+module("luci.controller.appropriate.box", package.seeall)
+
+local ddebug = require "luci.commotion.debugger"
+
+local path = {'/www', 'luci', 'media', 'app_box' }
 
 function index()
-   entry({"media", "viewer"}, call("prepare_box"), "Commotion Media Server", 10).dependent = false
-   entry({"media", "download"}, call("download"), "Commotion Media Server Download").dependent = false
+   entry({'commotion', 'appropriate'}, call("prepare_box"), "Commotion Media Server", 10).dependent = false
 end
 
 function prepare_box()
    local sys = require "luci.sys"
    local fs = require "luci.fs"
-   
-   local file_permission = nixio.fs.access("/www/luci/media/pirate")
-   local pirate_folder = "/www/luci/media/pirate/"
-   
-   -- causes media files to be uploaded to their namesake on the server.
-   local fp
-   luci.http.setfilehandler(
-	  function(meta, chunk, eof)
-		 if not fp then
-			if meta and meta.name == "file" then
-			   fp = io.open(pirate_folder .. meta.file, "w")
-			end
-		 end
-		 if chunk then
-			fp:write(chunk)
-		 end
-		 if eof then
-			fp:close()
-		 end
-		 end
-						   )   
-   --
-   -- Load luci-template
-   --
-   luci.template.render("pirate-box/mediasvr")
-end
+   local util = require "luci.commotion.util"
 
+   local filepath = table.concat( path, '/' )
+   ddebug.log("FIELPATH")
+   ddebug.log(filepath)
+  --in case it is not working check this variable
+   local file_permission = nixio.fs.access(filepath)
+   if not file_permission then
+	  ddebug.log("The appropriate box is broken because you don't have permission.")
+   end
+ 
+   -- causes media files to be uploaded to their namesake on the server.
+   util.upload(filepath.."/", "upload")
+
+   if luci.http.formvalue("file") then
+	  download()
+   end
+   
+   luci.template.render("appropriate/box", {path=path, filepath=filepath})
+end
 
 function download()
   filename = luci.http.formvalue("file")
 
   -- no file name provided
-
   if not filename then
     luci.http.status(403, "Forbidden")
     return
   end
 
   -- no relative paths with backrefs
-
   if filename:find("%.%.") then
     luci.http.status(403, "Access denied")
     return
   end
 
   -- no absolute paths
-
   if # filename > 0 and filename:sub(1,1) == '/' then
     luci.http.status(403, "Access denied")
     return
   end
 
-  local pirate_folder = "/www/luci/media/pirate/"
-  local f = io.open(pirate_folder .. filename)
+  local folder = table.concat( path, '/' )
+  local f = io.open(folder .. filename)
 
   -- file does not exist
 
